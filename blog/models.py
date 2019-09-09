477db275d5c051177756db1117b320ce3870b2d9
Matthew Guerrero <44419303+mathguerrero@users.noreply.github.com>
2019-09-09 01:19:25 -0700

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,37 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core.validators import RegexValidator
+
+spotify_regex = RegexValidator(regex=r'(https://open.spotify.com/){1}(track|album|artist){1}/.*',
+    message="Please enter a valid link.")
+
+int_choices = [(i,i) for i in range(1,11)]
+
+cat_choices = [
+    ("Song", "Song"),
+    ("Album", "Album"),
+    ("Artist", "Artist"),
+]
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField()
-    date = models.DateTimeField(default=timezone.now())
+    category = models.CharField(choices=cat_choices, max_length=50)
+    title = models.CharField("Name",max_length=50)
+    
+    rating = models.IntegerField(choices=int_choices)
+    
+    content = models.TextField("Tell us about it!")
+    date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    link = models.CharField("Spotify Link", validators=[spotify_regex], 
+            max_length=100, 
+            help_text="URL format: 'https://open.spotify.com/(track|album|artist)/.*'")
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
